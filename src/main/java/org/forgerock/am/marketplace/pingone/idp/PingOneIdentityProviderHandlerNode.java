de1e496e3dc5ce26f7b28671ab08679f4a39728c
Peter Holko <peterholko@pingidentity.com>
2024-09-04 12:56:37 -0700

    Reverting the hostname from the test hostname

--- src/main/java/org/forgerock/am/marketplace/pingone/idp/PingOneIdentityProviderHandlerNode.java
+++ src/main/java/org/forgerock/am/marketplace/pingone/idp/PingOneIdentityProviderHandlerNode.java
@@ -265,8 +265,7 @@ public class PingOneIdentityProviderHandlerNode extends AbstractSocialProviderHa
     }
 
     // create the PAR request and send it
-    // URI uri = URI.create(getPingOneBaseUrl(tntpPingOneConfig) + "/par");
-    URI uri = URI.create("http://davincitreenode.ping-devops.com:3000/par");
+    URI uri = URI.create(getPingOneBaseUrl(tntpPingOneConfig) + "/par");
     Request request = null;
 
     try {
